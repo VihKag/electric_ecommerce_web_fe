b1425e691fe7f2c33e60ffaab8554788b79d984f
VihKag <nvkhang191@gmail.com>
2024-08-06 18:36:35 +0700

    init auth routes

--- src/route/auth.js
+++ src/route/auth.js
@@ -1,14 +1,19 @@
-import React, { memo } from "react";
+import { Spin } from "antd";
+import React, { lazy, memo } from "react";
 import { Route, Routes } from "react-router-dom";
+import AuthenLayout from "../layout/AuthenLayout";
+const Login = lazy(() => import("../pages/auth/Login"));
+// const ForgotPass = lazy(() => import('../pages/auth/ForgotPass'));
+// const SignUp = lazy(() => import('../pages/auth/SignUp'));
+// const FbSignUp = lazy(() => import('../pages/auth/FbSignUp'));
+// const FbLogin = lazy(() => import('../pages/auth/FbLogin'));
 
-const AuthRoutes = memo(() => {
+export const AuthenRoutes = () => {
   return (
     <Routes>
-      <Route index path="/login" element={<Login />} />
-      <Route path="forgotPassword" element={<ForgotPass />} />
-      <Route path="register" element={<SignUp />} />
-      <Route path="fbRegister" element={<FbSignUp />} />
-      <Route path="fbLogIn" element={<FbLogin />} />
+      <Route element={<AuthenLayout />}>
+        <Route path="/login" element={<Login />} />
+      </Route>
     </Routes>
   );
-});
+};
