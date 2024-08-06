545f0893376fc421cd66be44338c5ba67980965d
VihKag <nvkhang191@gmail.com>
2024-08-06 18:35:03 +0700

    update

--- webpack.config.js
+++ webpack.config.js
@@ -5,28 +5,51 @@ module.exports = {
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
+    // filename: 'bundle.js',
     filename: 'bundle.js',
+    publicPath: '/',
   },
   module: {
     rules: [
       {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
+        test: /\.(js|jsx)$/, // Apply this rule to .js and .jsx files
+        exclude: /node_modules/, // Exclude node_modules directory
         use: {
-          loader: 'babel-loader',
+          loader: 'babel-loader',// Use Babel to transpile JavaScript
         },
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader', 'postcss-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(csv|tsv)$/i,
+        use: ['csv-loader'],
+      },
+      {
+        test: /\.xml$/i,
+        use: ['xml-loader'],
       },
     ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './public/index.html',
+      template: './public/index.html',// Template HTML file
     }),
   ],
   devServer: {
     static: {
-      directory: path.join(__dirname, 'public'),
+      directory: path.join(__dirname, 'public'),// Serve static files from the public directory
     },
-    port: 3000,
+    port: 3000, // Development server port
+    historyApiFallback: true, // Fallback to index.html for single-page applications
   },
 };
