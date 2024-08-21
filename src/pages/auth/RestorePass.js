269a1e3a91500706df24b8ecfd4ac4fef859ee3f
VihKag <nvkhang191@gmail.com>
2024-08-21 16:49:38 +0700

    init

---
+++ src/pages/auth/RestorePass.js
@@ -0,0 +1,55 @@
+import React from "react";
+import LogoShop from "../../assets/image/electric.jpg";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faChevronLeft } from "@fortawesome/free-solid-svg-icons";
+import { useNavigate, useNavigation } from "react-router-dom";
+export default function RestorePassword() {
+  const navigate = useNavigate();
+  const handleBack = () => {
+    navigate("/login");
+  };
+  return (
+    <>
+      <div className="max-w-[700px] min-w-[280px] mx-auto pb-10">
+        <div className="mx-auto my-6 ">
+          <div
+            className="turn-back h-8 w-8 hover:cursor-pointer"
+            onClick={handleBack}
+          >
+            <FontAwesomeIcon icon={faChevronLeft} />
+          </div>
+          <img
+            className="mx-auto w-32 h-32 rounded-full"
+            src={LogoShop}
+            alt="Logo web"
+          />
+          <h2 className="mx-auto w-max">Quên mật khẩu</h2>
+        </div>
+        <form>
+          <div className="mx-auto mb-4">
+            <label
+              htmlFor="email"
+              className="mb-2.5 block font-medium text-black"
+            >
+              Email
+            </label>
+            <div className="relative">
+              <input
+                type="text"
+                placeholder="Nhập email"
+                required
+                className="w-full rounded-lg border bg-transparent py-4 pl-6 pr-10 text-black outline-none focus:border-primary focus-visible:shadow-none"
+              />
+            </div>
+          </div>
+          <button
+            type="submit"
+            className="w-full cursor-pointer rounded-lg border border-blue-500 bg-blue-500 p-4 text-white font-bold transition hover:bg-opacity-90"
+          >
+            Reset Password
+          </button>
+        </form>
+      </div>
+    </>
+  );
+}
