89825b0849ad06de38e1b519e9168e565e2b0f41
VihKag <nvkhang191@gmail.com>
2024-08-21 19:52:59 +0700

    rename authen to AuthenRoute

--- src/route/AuthRoute.js
+++ src/route/AuthRoute.js
@@ -1,19 +1,20 @@
-import { Spin } from "antd";
 import React, { lazy, memo } from "react";
 import { Route, Routes } from "react-router-dom";
 import AuthenLayout from "../layout/AuthenLayout";
 const Login = lazy(() => import("../pages/auth/Login"));
-// const ForgotPass = lazy(() => import('../pages/auth/ForgotPass'));
+const Register = lazy(() => import("../pages/auth/Register"));
 // const SignUp = lazy(() => import('../pages/auth/SignUp'));
 // const FbSignUp = lazy(() => import('../pages/auth/FbSignUp'));
 // const FbLogin = lazy(() => import('../pages/auth/FbLogin'));
-
-export const AuthenRoutes = () => {
+const RestorePass = lazy(() => import('../pages/auth/RestorePass'));
+export const AuthenRoutes = memo(()=>{
   return (
     <Routes>
       <Route element={<AuthenLayout />}>
-        <Route path="/login" element={<Login />} />
+        <Route exact path="/login" element={<Login />} />
+        <Route exact path="/register" element={<Register />} />
+        <Route exact path="/restore-password" element={<RestorePass />} />
       </Route>
     </Routes>
   );
-};
+});
