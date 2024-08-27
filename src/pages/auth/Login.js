6f10212ac8ca6726bc92db100dcf29a3fd5552fd
VihKag <nvkhang191@gmail.com>
2024-08-27 16:31:14 +0700

    fix UI/UX

--- src/pages/auth/Login.js
+++ src/pages/auth/Login.js
@@ -1,9 +1,9 @@
 import { faEye, faEyeSlash, faUser } from "@fortawesome/free-regular-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import React, {useState} from "react";
+import React, { memo, useState } from "react";
 import { Link } from "react-router-dom";
 import LogoGG from "../../assets/icon/logoGG.png";
-import LogoShop from "../../assets/image/electric.jpg";
+import LogoFb from "../../assets/icon/logoFb.png";
 
 function Login() {
   const [frmLogin, setFrmLogin] = useState({
@@ -17,76 +17,95 @@ function Login() {
   return (
     <>
       <div className="mx-auto my-6">
-        <img className="mx-auto w-32 h-32 rounded-full" src={LogoShop} alt="Logo web" />
+        {/* <img className="mx-auto w-32 h-32 rounded-full" src={LogoShop} alt="Logo web" /> */}
         <h2 className="mx-auto w-max">Đăng nhập</h2>
       </div>
-      <form className="max-w-[700px] min-w-[280px] mx-auto">
-        <div className="mb-4">
-          <label className="mb-2.5 block font-medium text-black">
-            Tài khoản
-          </label>
-          <div className="relative">
-            <input
-              type="text"
-              placeholder="Nhập tài khoản"
-              required
-              className="w-full rounded-lg border border-stroke bg-transparent py-4 pl-6 pr-10 text-black outline-none focus:border-primary focus-visible:shadow-none"
-            />
-            <span className="absolute right-4 top-4">
-              <FontAwesomeIcon icon={faUser} className="text-gray-400" />
+
+      <div className="max-w-[700px] min-w-[280px] mx-auto pb-10">
+        <div className="flex items-center gap-4 justify-center">
+          <button className="flex w-48 h-14 items-center justify-center gap-3.5 rounded-lg border-2 border-stroke bg-gray p-4 font-bold hover:bg-opacity-50">
+            <span>
+              <img src={LogoGG} alt="logo google" className="h-6" />
             </span>
-          </div>
-        </div>
+            Google
+          </button>
 
-        <div className="mb-4">
-          <label className="mb-2.5 block font-medium text-black">
-            Mật khẩu
-          </label>
-          <div className="relative">
-            <input
-              type={showPassword ? "text" : "password"}
-              id="password"
-              name="password"
-              autoComplete="on"
-              placeholder="Nhập mât khẩu"
-              required
-              className="w-full rounded-lg border border-stroke bg-transparent py-4 pl-6 pr-10 text-black outline-none focus:border-primary focus-visible:shadow-none"
-            />
-            <span className="absolute right-4 top-4 cursor-pointer">
-              <FontAwesomeIcon onClick={togglePasswordVisibility} icon={showPassword ? faEye: faEyeSlash} className="text-gray-400" id="icon-password" />
+          <button className="flex w-48 h-14 items-center justify-center rounded-lg border-2 border-stroke bg-gray p-4 font-bold hover:bg-opacity-50">
+            <span>
+              <img src={LogoFb} alt="logo google" className="h-6" />
             </span>
+            Facebook
+          </button>
+        </div>
+
+        <div className="gap-2 flex items-center mx-auto w-3/5 my-8">
+          <hr className="w-full h-[1.5px] bg-gray-300" />
+          hoặc
+          <hr className="w-full h-[1.5px] bg-gray-300" />
+        </div>
+
+        <form>
+          <div className="mb-4">
+            <label className="mb-2.5 block font-medium text-black">
+              Tài khoản
+            </label>
+            <div className="relative">
+              <input
+                type="text"
+                placeholder="Nhập tài khoản"
+                required
+                className="w-full rounded-lg border border-stroke bg-transparent py-4 pl-6 pr-10 text-black outline-none focus:border-primary focus-visible:shadow-none"
+              />
+              <span className="absolute right-4 top-4">
+                <FontAwesomeIcon icon={faUser} className="text-gray-400" />
+              </span>
+            </div>
           </div>
-          <div className="text-secondary m-2 justify-end flex">
-            <Link to="/restore-password">quên mật khẩu?</Link>
+
+          <div className="mb-4">
+            <label className="mb-2.5 block font-medium text-black">
+              Mật khẩu
+            </label>
+            <div className="relative">
+              <input
+                type={showPassword ? "text" : "password"}
+                name="password"
+                autoComplete="on"
+                placeholder="Nhập mât khẩu"
+                required
+                className="w-full rounded-lg border border-stroke bg-transparent py-4 pl-6 pr-10 text-black outline-none focus:border-primary focus-visible:shadow-none"
+              />
+              <span className="absolute right-4 top-4 cursor-pointer">
+                <FontAwesomeIcon
+                  onClick={togglePasswordVisibility}
+                  icon={showPassword ? faEye : faEyeSlash}
+                  className="text-gray-400"
+                  id="icon-password"
+                />
+              </span>
+            </div>
+            <div className="text-secondary m-2 justify-end flex hover:text-hover">
+              <Link to="/restore-password">quên mật khẩu?</Link>
+            </div>
           </div>
-        </div>
+          <div>
+            <input
+              type="submit"
+              value="Đăng nhập"
+              className="w-full cursor-pointer rounded-lg border border-primary bg-primary p-4 text-white font-bold transition hover:bg-opacity-90"
+            />
+          </div>
 
-
-
-        <div className="mb-5">
-          <input
-            type="submit"
-            value="Đăng nhập"
-            className="w-full cursor-pointer rounded-lg border border-blue-500 bg-blue-500 p-4 text-white font-bold transition hover:bg-opacity-90"
-          />
-        </div>
-
-        <button className="flex w-full items-center justify-center gap-3.5 rounded-lg border border-stroke bg-gray p-4 font-bold hover:bg-opacity-50">
-          <span>
-            <img src={LogoGG} alt="logo google" className="w-6 h-6" />
-          </span>
-          Đăng nhập với Google
-        </button>
-
-        <div className="mt-6 text-center">
-          <p>
-            Bạn chưa có tài khoản?{" "}
-            <Link to="/register" className="text-blue-500 font-bold">
-              Đăng ký
-            </Link>
-          </p>
-        </div>
-      </form>
+          <div className="mt-6 text-center">
+            <p>
+              Bạn chưa có tài khoản?{" "}
+              <Link to="/register" className="text-blue-500 font-bold">
+                Đăng ký
+              </Link>
+            </p>
+          </div>
+        </form>
+      </div>
     </>
   );
 }
--- src/pages/auth/Register.js
+++ src/pages/auth/Register.js
@@ -2,128 +2,147 @@ import { faEyeSlash, faUser } from "@fortawesome/free-regular-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
 import { Link } from "react-router-dom";
-import LogoGG from "../../assets/icon/logoGG.png"
-import LogoShop from "../../assets/image/electric.jpg";
+import LogoGG from "../../assets/icon/logoGG.png";
+import { faPhone, faPhoneFlip } from "@fortawesome/free-solid-svg-icons";
+import LogoFb from "../../assets/icon/logoFb.png";
+// import LogoShop from "../../assets/image/electric.jpg";
 function Register() {
   return (
     <>
       <div className="mx-auto my-6">
-        <img className="mx-auto w-32 h-32 rounded-full" src={LogoShop} alt="Logo web"/>
+        {/* <img className="mx-auto w-32 h-32 rounded-full" src={LogoShop} alt="Logo web"/> */}
         <h2 className="mx-auto w-max">Đăng ký</h2>
       </div>
-      <form className="max-w-[700px] min-w-[280px] mx-auto pb-10">
-      <div className="mb-4">
-          <label className="mb-2.5 block font-medium text-black">
-            Họ tên
-          </label>
-          <div className="relative">
-            <input
-              type="text"
-              placeholder="Nhập tài khoản"
-              required
-              
-              className="w-full rounded-lg border border-stroke bg-transparent py-4 pl-6 pr-10 text-black outline-none focus:border-primary focus-visible:shadow-none"
-            />
-            <span className="absolute right-4 top-4">
-              <FontAwesomeIcon icon={faUser} className="text-gray-400" />
+
+      <div className="max-w-[700px] min-w-[280px] mx-auto pb-10">
+        <div className="flex items-center gap-4 justify-center">
+          <button className="flex w-48 h-14 items-center justify-center gap-3.5 rounded-lg border-2 border-stroke bg-gray p-4 font-bold hover:bg-opacity-50">
+            <span>
+              <img src={LogoGG} alt="logo google" className="h-6" />
             </span>
-          </div>
-        </div>
+            Google
+          </button>
 
-        <div className="mb-4">
-          <label className="mb-2.5 block font-medium text-black">
-            Số điện thoại
-          </label>
-          <div className="relative">
-            <input
-              type="tel"
-              placeholder="Nhập tài khoản"
-              required
-              
-              className="w-full rounded-lg border border-stroke bg-transparent py-4 pl-6 pr-10 text-black outline-none focus:border-primary focus-visible:shadow-none"
-            />
-            <span className="absolute right-4 top-4">
-              <FontAwesomeIcon icon={faUser} className="text-gray-400" />
+          <button className="flex w-48 h-14 items-center justify-center rounded-lg border-2 border-stroke bg-gray p-4 font-bold hover:bg-opacity-50">
+            <span>
+              <img src={LogoFb} alt="logo google" className="h-6" />
             </span>
-          </div>
+            Facebook
+          </button>
         </div>
 
-        <div className="mb-4">
-          <label className="mb-2.5 block font-medium text-black">
-            Tài khoản
-          </label>
-          <div className="relative">
+        <div className="gap-2 flex items-center mx-auto w-3/5 my-8">
+          <hr className="w-full h-[1.5px] bg-gray-300" />
+          hoặc
+          <hr className="w-full h-[1.5px] bg-gray-300" />
+        </div>
+
+        <form>
+          <div className="mb-4">
+            <label className="mb-2.5 block font-medium text-black">
+              Họ tên
+            </label>
+            <div className="relative">
+              <input
+                type="text"
+                placeholder="Nhập tài khoản"
+                required
+                className="w-full rounded-lg border border-stroke bg-transparent py-4 pl-6 pr-10 text-black outline-none focus:border-primary focus-visible:shadow-none"
+              />
+            </div>
+          </div>
+
+          <div className="mb-4">
+            <label className="mb-2.5 block font-medium text-black">
+              Số điện thoại
+            </label>
+            <div className="relative">
+              <input
+                type="tel"
+                placeholder="Nhập tài khoản"
+                required
+                className="w-full rounded-lg border border-stroke bg-transparent py-4 pl-6 pr-10 text-black outline-none focus:border-primary focus-visible:shadow-none"
+              />
+              <span className="absolute right-4 top-4">
+                <FontAwesomeIcon icon={faPhone} className="text-gray-400" />
+              </span>
+            </div>
+          </div>
+
+          <div className="mb-4">
+            <label className="mb-2.5 block font-medium text-black">
+              Tài khoản
+            </label>
+            <div className="relative">
+              <input
+                type="text"
+                placeholder="Nhập tài khoản"
+                required
+                className="w-full rounded-lg border border-stroke bg-transparent py-4 pl-6 pr-10 text-black outline-none focus:border-primary focus-visible:shadow-none"
+              />
+              <span className="absolute right-4 top-4">
+                <FontAwesomeIcon icon={faUser} className="text-gray-400" />
+              </span>
+            </div>
+          </div>
+
+          <div className="mb-4">
+            <label className="mb-2.5 block font-medium text-black">
+              Mật khẩu
+            </label>
+            <div className="relative">
+              <input
+                type="password"
+                name="password"
+                autoComplete="on"
+                placeholder="Nhập mât khẩu"
+                required
+                className="w-full rounded-lg border border-stroke bg-transparent py-4 pl-6 pr-10 text-black outline-none focus:border-primary focus-visible:shadow-none"
+              />
+              <span className="absolute right-4 top-4 cursor-pointer">
+                <FontAwesomeIcon icon={faEyeSlash} className="text-gray-400" />
+              </span>
+            </div>
+          </div>
+
+          <div className="mb-4">
+            <label className="mb-2.5 block font-medium text-black">
+              Nhập lại mật khẩu
+            </label>
+            <div className="relative">
+              <input
+                type="password"
+                id="password"
+                name="password"
+                autoComplete="on"
+                placeholder="Nhập lại mât khẩu"
+                required
+                className="w-full rounded-lg border border-stroke bg-transparent py-4 pl-6 pr-10 text-black outline-none focus:border-primary focus-visible:shadow-none"
+              />
+              <span className="absolute right-4 top-4 cursor-pointer">
+                <FontAwesomeIcon icon={faEyeSlash} className="text-gray-400" />
+              </span>
+            </div>
+          </div>
+
+          <div className="mb-4">
             <input
-              type="text"
-              placeholder="Nhập tài khoản"
-              required
-              
-              className="w-full rounded-lg border border-stroke bg-transparent py-4 pl-6 pr-10 text-black outline-none focus:border-primary focus-visible:shadow-none"
+              type="submit"
+              value="Đăng ký"
+              className="w-full cursor-pointer rounded-lg border border-primary bg-primary p-4 text-white font-bold transition hover:bg-opacity-90"
             />
-            <span className="absolute right-4 top-4">
-              <FontAwesomeIcon icon={faUser} className="text-gray-400" />
-            </span>
           </div>
-        </div>
 
-        <div className="mb-6">
-          <label className="mb-2.5 block font-medium text-black">
-            Mật khẩu
-          </label>
-          <div className="relative">
-            <input
-              type="password"
-              id="password"
-              name="password"
-              autoComplete="on"
-              placeholder="Nhập mât khẩu"
-              required
-              className="w-full rounded-lg border border-stroke bg-transparent py-4 pl-6 pr-10 text-black outline-none focus:border-primary focus-visible:shadow-none"
-            />
-            <span className="absolute right-4 top-4 cursor-pointer">
-              <FontAwesomeIcon icon={faEyeSlash} className="text-gray-400" />
-            </span>
+          <div className="mt-6 text-center">
+            <p>
+              Bạn đã có tài khoản?{" "}
+              <Link to="/login" className="text-blue-500 font-bold">
+                Đăng nhập
+              </Link>
+            </p>
           </div>
-        </div>
-
-        <div className="mb-6">
-          <label className="mb-2.5 block font-medium text-black">
-            Nhập lại mật khẩu
-          </label>
-          <div className="relative">
-            <input
-              type="password"
-              id="password"
-              name="password"
-              autoComplete="on"
-              placeholder="Nhập lại mât khẩu"
-              required
-              className="w-full rounded-lg border border-stroke bg-transparent py-4 pl-6 pr-10 text-black outline-none focus:border-primary focus-visible:shadow-none"
-            />
-            <span className="absolute right-4 top-4 cursor-pointer">
-              <FontAwesomeIcon icon={faEyeSlash} className="text-gray-400" />
-            </span>
-          </div>
-        </div>
-
-
-        <div className="mb-5">
-          <input
-            type="submit"
-            value="Đăng ký"
-            className="w-full cursor-pointer rounded-lg border border-blue-500 bg-blue-500 p-4 text-white font-bold transition hover:bg-opacity-90"
-          />
-        </div>
-
-        <div className="mt-6 text-center">
-          <p>
-            Bạn đã có tài khoản?{" "}
-            <Link to="/login" className="text-blue-500 font-bold">
-              Đăng nhập
-            </Link>
-          </p>
-        </div>
-      </form>
+        </form>
+      </div>
     </>
   );
 }
