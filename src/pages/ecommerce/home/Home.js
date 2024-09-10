91858a09609be6f290e98e569781983c8aa5d5bf
VihKag <nvkhang191@gmail.com>
2024-09-10 20:21:27 +0700

    modify something changed

--- src/pages/ecommerce/home/Home.js
+++ src/pages/ecommerce/home/Home.js
@@ -1,17 +1,14 @@
 import React from "react";
 import Carousel from "../../../components/carousel/Carousel";
 import { Badge, Button, Card, Dropdown } from "antd";
-import menuData from "./menuData.json";
 import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faChevronRight,
-  faHeart,
-  faStar,
 } from "@fortawesome/free-solid-svg-icons";
 import SwiperWrapper from "../../../components/swiper/Swiper";
 import ProductCard from "../../../components/card/ProductCard";
-// import { faHeart } from "@fortawesome/free-regular-svg-icons";
+import {product} from "../../../data";
 const menu = [
   {
     title: "Điện thoại",
@@ -59,111 +56,8 @@ const menu = [
   }
 ];
 
-const product = [
-  {
-    discount: "18%",
-    image: "https://cdn2.cellphones.com.vn/insecure/rs:fill:358:358/q:90/plain/https://cellphones.com.vn/media/catalog/product/s/a/samsung-galaxy-z-flip-6-xanh-duong-6_2.png",
-    title: "Samsung Galaxy S24 Ultra 12GB 256GB",
-    price: "27.990.000\u0111",
-    oldPrice: "33.990.000\u0111",
-    sMemberDiscount: "280.000\u0111",
-    installment: "600.000\u0111",
-    rating: 5,
-  },
-  {
-    discount: "16%",
-    image: "https://cdn2.cellphones.com.vn/insecure/rs:fill:358:358/q:90/plain/https://cellphones.com.vn/media/catalog/product/s/a/samsung-galaxy-z-flip-6-xanh-duong-6_2.png",
-    title: "iPhone 15 Pro Max 256GB | Ch\u00EDnh h\u00E3ng VN/A",
-    price: "29.390.000\u0111",
-    oldPrice: "34.990.000\u0111",
-    sMemberDiscount: "294.000\u0111",
-    installment: "600.000\u0111",
-    rating: 5,
-  },
-  {
-    discount: "20%",
-    image: "https://cdn2.cellphones.com.vn/insecure/rs:fill:358:358/q:90/plain/https://cellphones.com.vn/media/catalog/product/s/a/samsung-galaxy-z-flip-6-xanh-duong-6_2.png",
-    title: "iPhone 13 128GB | Ch\u00EDnh h\u00E3ng VN/A",
-    price: "13.890.000\u0111",
-    oldPrice: "17.990.000\u0111",
-    sMemberDiscount: "139.000\u0111",
-    installment: "600.000\u0111",
-    rating: 5,
-  },
-  {
-    discount: "17%",
-    image: "https://cdn2.cellphones.com.vn/insecure/rs:fill:358:358/q:90/plain/https://cellphones.com.vn/media/catalog/product/s/a/samsung-galaxy-z-flip-6-xanh-duong-6_2.png",
-    title: "Samsung Galaxy M55 (12GB 256GB)",
-    price: "10.490.000\u0111",
-    oldPrice: "12.690.000\u0111",
-    sMemberDiscount: "105.000\u0111",
-    installment: "600.000\u0111",
-    rating: 5,
-  },
-  {
-    discount: "15%",
-    image: "https://cdn2.cellphones.com.vn/insecure/rs:fill:358:358/q:90/plain/https://cellphones.com.vn/media/catalog/product/s/a/samsung-galaxy-z-flip-6-xanh-duong-6_2.png",
-    title: "iPhone 15 128GB | Ch\u00EDnh h\u00E3ng VN/A",
-    price: "19.590.000\u0111",
-    oldPrice: "22.990.000\u0111",
-    sMemberDiscount: "196.000\u0111",
-    installment: "600.000\u0111",
-    rating: 5,
-  },
-  {
-    discount: "18%",
-    image: "https://cdn2.cellphones.com.vn/insecure/rs:fill:358:358/q:90/plain/https://cellphones.com.vn/media/catalog/product/s/a/samsung-galaxy-z-flip-6-xanh-duong-6_2.png",
-    title: "Xiaomi Redmi Note 13 Pro 4G",
-    price: "5.990.000\u0111",
-    oldPrice: "7.290.000\u0111",
-    sMemberDiscount: "60.000\u0111",
-    installment: "200.000\u0111",
-    rating: 5,
-  },
-  {
-    discount: "13%",
-    image: "https://cdn2.cellphones.com.vn/insecure/rs:fill:358:358/q:90/plain/https://cellphones.com.vn/media/catalog/product/s/a/samsung-galaxy-z-flip-6-xanh-duong-6_2.png",
-    title: "iPhone 15 Plus 128GB | Ch\u00EDnh h\u00E3ng VN/A",
-    price: "22.590.000\u0111",
-    oldPrice: "25.990.000\u0111",
-    sMemberDiscount: "226.000\u0111",
-    installment: "600.000\u0111",
-    rating: 5,
-  },
-  {
-    discount: "9%",
-    image: "https://cdn2.cellphones.com.vn/insecure/rs:fill:358:358/q:90/plain/https://cellphones.com.vn/media/catalog/product/s/a/samsung-galaxy-z-flip-6-xanh-duong-6_2.png",
-    title: "Xiaomi POCO M6 (6GB 128GB)",
-    price: "3.890.000\u0111",
-    oldPrice: "4.290.000\u0111",
-    sMemberDiscount: "37.000\u0111",
-    installment: "200.000\u0111",
-    rating: 5,
-  },
-  {
-    discount: "10%",
-    image: "https://cdn2.cellphones.com.vn/insecure/rs:fill:358:358/q:90/plain/https://cellphones.com.vn/media/catalog/product/s/a/samsung-galaxy-z-flip-6-xanh-duong-6_2.png",
-    title: "Xiaomi Redmi Note 13 (6GB 128GB)",
-    price: "4.390.000\u0111",
-    oldPrice: "4.890.000\u0111",
-    sMemberDiscount: "44.000\u0111",
-    installment: "200.000\u0111",
-    rating: 5,
-  },
-  {
-    discount: "3%",
-    image: "https://cdn2.cellphones.com.vn/insecure/rs:fill:358:358/q:90/plain/https://cellphones.com.vn/media/catalog/product/s/a/samsung-galaxy-z-flip-6-xanh-duong-6_2.png",
-    title: "Infinix Note 40 Pro 8GB 256GB",
-    price: "5.790.000\u0111",
-    oldPrice: "5.990.000\u0111",
-    sMemberDiscount: "58.000\u0111",
-    installment: "200.000\u0111",
-    rating: 5,
-  },
-];
 
 export default function Home() {
-  console.log(menuData.menuData);
   const slides = [
     "https://via.placeholder.com/600x400?text=Slide+1",
     "https://via.placeholder.com/600x400?text=Slide+2",
@@ -176,9 +70,9 @@ export default function Home() {
 
   return (
     <>
-      <div className="max-w-[1200px] mx-auto">
-        <div className="top-home h-[480px]  mx-auto flex transition-all">
-          <div className="main-menu w-1/5 hidden xs:block min-w-[180px] h-full rounded-md border shadow-lg duration-300">
+      <div className="max-w-[1200px] container">
+        <div className="top-home sm:h-[480px]  mx-auto flex transition-all">
+          <div className="main-menu w-1/5 hidden sm:block min-w-[180px] h-full rounded-md border shadow-lg duration-300">
             <div className="cascading-menu flex flex-col">
               {menu.map((item, index) => {
                 return (
@@ -203,8 +97,10 @@ export default function Home() {
         </div>
 
         <div className="block-featured-product my-4">
-          <div className="">
-            <div className="flex flex-wrap gap-2 mb-4">
+          <div>
+            <div className="flex justify-between items-center mb-4">
+            <h2 className="text-2xl font-semibold text-text">Featured Products</h2>
+            <div className="flex flex-wrap gap-2">
               {[
                 "Apple",
                 "Samsung",
@@ -217,12 +113,13 @@ export default function Home() {
                 "Nokia",
                 "Infinix",
                 "Oneplus",
-                "Xem t\u1EA5t c\u1EA3",
+                "Xem tất cả",
               ].map((brand) => (
-                <Button key={brand} variant="outline" className="px-4 py-2">
+                <Button key={brand} variant="" className="px-2 text-sm py-1 bg-gray-100 font-normal">
                   {brand}
                 </Button>
               ))}
+            </div>
             </div>
             <div>
               <SwiperWrapper items={featuredProducts} />
--- src/route/user/EcommerceRoute.js
+++ src/route/user/EcommerceRoute.js
@@ -1,13 +1,15 @@
 import React, { lazy, memo } from "react";
 import { Route, Routes } from "react-router-dom";
 import EcommerceLayout from "../../layout/EcommerceLayout";
-const Home = lazy(() => import("../../pages/ecommerce/home/index"));
+import Category from "../../pages/ecommerce/category/Category";
+const Home = lazy(() => import("../../pages/ecommerce/home/Home"));
 const EcommerceRoutes = memo(() => {
   return (
     <>
       <Routes>
         <Route element={<EcommerceLayout />}>
           <Route index element={<Home />}/>
+          <Route path="category" element={<Category/>}/>
         </Route>
       </Routes>
     </>
