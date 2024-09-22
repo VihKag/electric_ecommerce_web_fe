82166e9bbcf865e4a4110abfd3b3765ae239b0bd
ViKa <nvkhang191l@gmail.com>
2024-09-22 14:19:40 +0700

    fix navbar

--- src/layout/AuthenLayout.js
+++ src/layout/AuthenLayout.js
@@ -1,14 +1,14 @@
 import React, { Suspense } from "react";
 import { Outlet } from "react-router-dom";
 import { Spin } from "antd";
-import ClientHeader from "../components/header/ClientHeader";
+import ClientNav from "../components/navbar/ClientNav";
 function AuthenLayout() {
   return (
     <>
       <Suspense fallback={<Spin/>}>
         <div className="overflow-auto h-screen">
           <header className="bg-background border-b shadow-sm sticky top-0 z-30 flex justify-center">
-            <ClientHeader/>
+            <ClientNav/>
           </header>
           <main className="container mx-auto px-4 mt-8">
             <Outlet />
--- src/layout/ClientLayout.js
+++ src/layout/ClientLayout.js
@@ -1,14 +1,14 @@
 import React, { Suspense } from "react";
 import { Outlet } from "react-router-dom";
 import { Spin } from "antd";
-import ClientHeader from "../components/header/ClientHeader";
+import ClientNav from "../components/navbar/ClientNav";
 function ClientLayout() {
   return (
     <>
       <Suspense fallback={<Spin/>}>
         <div className="overflow-auto h-screen">
           <header className="bg-sale border-b shadow-sm sticky top-0 z-30 flex justify-center mb-2">
-            <ClientHeader/>
+            <ClientNav/>
           </header>
           <main>
             <Outlet />
