223933cbe75cb132b341b89d5ae9372dad44d5df
ViKa <nvkhang191l@gmail.com>
2024-10-25 06:22:53 +0700

    remove headers config api, add api

--- src/services/apiService.js
+++ src/services/apiService.js
@@ -2,19 +2,21 @@ import axios from "axios";
 const api = axios.create(
     {
         baseURL : 'http://127.0.0.1:4000',
-        headers: {
-            'Content-Type': 'application/json',
-        },
         // withCredentials: true, // To send cookies with requests
     }
 )
 
 export const authService = {
-    clientLogin:(credentials) => api.post('/user/login',credentials),
-    register:(userInfor)=>  api.post('/user/register',userInfor),
-    verifyEmail: (email) => api.post(`/user/email_verification/${email}`),
-    sendOTP:()=> api.post('/user/verify1'),
-    resetPassword:()=> api.post('/user/resetpass')
+    clientLogin:(credentials) => api.post('/users/login',credentials),
+    register:(userInfor)=>  api.post('/users/register',userInfor),
+    verifyEmail: (email) => api.post(`/users/email_verification/${email}`),
+    sendOTP:() => api.post('/users/verify1'),
+    resetPassword:() => api.post('/users/resetpass'),
+    authToken:(token) => api.post('/users/logintoken',null,{
+        headers:{
+            'token': `Baerer ${token}`,
+        }
+    }),
 }
 
 export const productService = {
