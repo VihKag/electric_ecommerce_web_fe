75bdc005219c0f8edc353d745797b433a19a3f14
ViKa <nvkhang191l@gmail.com>
2024-10-27 14:33:42 +0700

    add admin routes

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { AuthenRoutes } from "./route/AuthRoute";
 import ClientRoutes from "./route/client/ClientRoutes";
+import AdminRoutes from "./route/admin/AdminRoutes";
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="/auth/*" element={<AuthenRoutes />} />
+        <Route path="/admin/*" element={<AdminRoutes />} />
         <Route path="/*" element={<ClientRoutes />} />
       </Routes>
     </>
