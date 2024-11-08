da158687f93c82b3f0884709be135d4100422487
ViKa <nvkhang191l@gmail.com>
2024-11-08 14:53:42 +0700

    delete config token

--- src/themeConfig.js
+++ src/themeConfig.js
@@ -1,9 +1,7 @@
 // themeConfig.js
 const themeConfig = {
     token: {
-      colorPrimary: '#ff0000', // Màu đỏ
-      colorTextHovered: '#ff0000',
-      colorTextDisabled: '#ff9999', // Màu khi bị disabled
+
     },
   };
   
