4b496f64faa4cb3f9907b806e386194eb3824e3d
ViKa <nvkhang191l@gmail.com>
2024-11-12 14:54:11 +0700

    init function

---
+++ src/utils/convertUltils.js
@@ -0,0 +1,9 @@
+export function convertToSlug(text) {
+    return text
+      .toLowerCase()
+      .normalize("NFD")
+      .replace(/[\u0300-\u036f]/g, "")
+      .replace(/\s+/g, "-")
+      .replace(/[^a-z0-9-]/g, "");
+  }
+  
---
+++ src/utils/currencyUtils.js
@@ -0,0 +1,8 @@
+export function formatCurrency(number) {
+    return new Intl.NumberFormat('vi-VN', {
+      style: 'currency',
+      currency: 'VND',
+      minimumFractionDigits: 0
+    });
+  }
+  
