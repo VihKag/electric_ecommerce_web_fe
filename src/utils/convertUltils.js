ab7c68be1e15db183a1a76eae6b2abfb17f98bf1
ViKa <nvkhang191l@gmail.com>
2024-11-30 17:31:14 +0700

    update ultils

--- src/utils/convertUltils.js
+++ src/utils/convertUltils.js
@@ -1,9 +1,18 @@
 export function convertToSlug(text) {
-    return text
-      .toLowerCase()
-      .normalize("NFD")
-      .replace(/[\u0300-\u036f]/g, "")
-      .replace(/\s+/g, "-")
-      .replace(/[^a-z0-9-]/g, "");
+  return text
+    .replace(/Đ/g, "d")
+    .toLowerCase()
+    .replace(/đ/g, "d")
+    .normalize("NFD")
+    .replace(/[\u0300-\u036f]/g, "")
+    .replace(/\s+/g, "-")
+    .replace(/[^a-z0-9-]/g, "");
+}
+
+export function toUpperCase(str) {
+  if (typeof str !== "string") {
+    throw new Error("Input must be a string");
   }
-  
+  return str.toUpperCase();
+}
+
--- src/utils/currencyUtils.js
+++ src/utils/currencyUtils.js
@@ -1,8 +1,11 @@
 export function formatCurrency(number) {
-    return new Intl.NumberFormat('vi-VN', {
-      style: 'currency',
-      currency: 'VND',
-      minimumFractionDigits: 0
-    });
-  }
-  
+  // Format số theo kiểu tiền tệ VND
+  const formatted = new Intl.NumberFormat('vi-VN', {
+    style: 'currency',
+    currency: 'VND',
+    minimumFractionDigits: 0
+  }).format(number);
+
+  // Xóa khoảng trắng giữa số và ký hiệu tiền tệ (₫)
+  return formatted.replace(/\s₫/, '₫');
+}
