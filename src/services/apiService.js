4679a7928b1ef5fd559351ef93329901a9dc8f77
ViKa <nvkhang191l@gmail.com>
2024-12-17 04:11:51 +0700

    update api

--- src/services/apiService.js
+++ src/services/apiService.js
@@ -1,28 +1,32 @@
 import axios from "axios";
 import { get } from "react-hook-form";
+const tkToken = localStorage.getItem("tk");
+const acToken = localStorage.getItem("access_token");
 const api = axios.create(
     {
-        baseURL : 'http://127.0.0.1:4000',
+        baseURL: "https://techzone-2ow9.onrender.com",
+        headers: {
+            Authorization: `Bearer ${acToken}`,
+        },
     }
 )
-// Thêm interceptor để tự động thêm token vào header Authorization
-// api.interceptors.request.use(
-//     (config) => {
-//       const token = localStorage.getItem('tk'); // Lấy token từ localStorage
-//       if (token) {
-//         config.headers['Authorization'] = `Bearer ${token}`;
-//       }
-//       return config;
-//     },
-//     (error) => Promise.reject(error)
-//   );
+
+
+
+export const employeeServiceApi = axios.create({
+    baseURL: "https://techzone-2ow9.onrender.com",
+    headers: {
+        Authorization: `Bearer ${tkToken}`,
+    },
+});
+
 
 export const authService = {
     clientLogin:(credentials) => api.post('/users/login',credentials),
     register:(userInfor)=>  api.post('/users/register',userInfor),
     verifyEmail: (email) => api.post(`/users/email_verification/${email}`),
     sendOTP:(email,otp) => api.post('/users/verify1', {email, otp}),
-    resetPassword:() => api.post('/users/resetpass'),
+    resetPassword:(data) => api.post('/users/resetpass',data),
     authToken:(token) => api.post('/users/logintoken',null,{
         headers:{
             Authorization: `Bearer ${token}`,
@@ -46,11 +50,16 @@ export const productService = {
     }),
     getProductById: (id) => api.get(`/products/${id}`),
     addProduct: (product) => api.post('/products/create', product),
-    updateProduct: (product) => api.put(`/products`, product),
+    updateProduct: (product) => api.put(`/products`, product, {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        },
+      }),
     deleteProduct: (id) => api.delete(`/products/${id}`),
     getHomePage: () => api.get('/products/home'),
     getRelativeProducts: (id) => api.get(`/products//${id}/relatedproduct`),
     getSearchedProducts: (params) =>api.get(`/products/search`, params),
+    
 }
 
 export const brandService = {
@@ -65,7 +74,7 @@ export const brandService = {
           'Content-Type': 'multipart/form-data',
         },
       }),
-      deleteBrandById: (id) => api.delete(`/brands/${id}`),
+    deleteBrandById: (id) => api.delete(`/brands/${id}`),
 }
 
 export const categoryService = {
@@ -89,13 +98,34 @@ export const categoryService = {
 
 export const commonService = {
     getBanners: () => api.get('/banners'),
+    deleteBanner: (id) => api.delete(`/banners/${id}`),
     getReviewsByProductId: (productId) => api.get(`/reviews/${productId}`),
     addReviews: (review) => api.post('/reviews', review),
-    getAddresseByUserId: (id) => api.get(`/address/${id}/getdefault`),
+    getAddresseByUserIdDefault: () => api.get(`/address/getdefault`),
+    getAddresseByUserId: (userId) => api.get(`/address`,userId),
+    getOrderByUserId: () => api.get(`/orders`),
+    createOrder: (order) => api.post('/orders', order),
+    createAddress: (address) => api.post('/address/create', address),
+    deleteAddress: (userId, addressId) => api.delete(`/address/user/${userId}/location/${addressId}`),
+    updateAddress: (data) => api.put('/address', data),
+    likeProduct: (data) => api.put('/favourites', data),
+    getFavouriteProduct: (userId) => api.get(`/favourites/products/${userId}`),
 }
 
+
+export const bannerService = {
+    getBanners: () => api.get(`/banners`),
+    updateBanners: (formdata) => employeeServiceApi.put('/banners',formdata,{
+        headers: { "Content-Type": "multipart/form-data" },
+      }),
+    createBanners: (formdata) => employeeServiceApi.post('/banners/create', formdata,{
+        headers: { "Content-Type": "multipart/form-data" },
+      }),
+}
+
+
 export const cartService = {
-    getCartByUserId: (id) => api.get(`/carts/${id}/user`),
+    getCartByUserId: () => api.get(`/carts/user`),
     addProductToCart: (user, product, quantity, memory, color) => api.put("/carts", {user, product, quantity, memory, color}),
     updateCart: (cartData)=> api.put("/carts", cartData),
     removeProductFromCart: (user, product) => api.post("/carts", {user, product}),
@@ -103,22 +133,35 @@ export const cartService = {
 
 }
 
+export const filtersService = {
+    getFilters: () => api.get("/filters"),
+    createFilters: (values) => api.post(`/filters`, values),
+    updateFilters: (id, values) => api.put(`/filters/${id}`, values),
+}
+
 export const adminService = {
-    getAllProducts: () => api.get('/employees/products'),
-    getProductById: (id) => api.get(`/employees/products/${id}`),
-    getCategories: (config) => api.get('/employees/categories',config),
-    getBrands: (config) => api.get('/employees/brands',config),
-    getUsers: (config) => api.get('/employees/users',config),
-    getStaffs: (config) => api.get('/employees/staffs',config),
-    getOrders: (config) => api.get('/employees/orders',config),
-    postProducts: (productData) => api.post('/employees/products', productData,
+    getAllProducts: () => employeeServiceApi.get('/employees/products'),
+    getAllProductOnSales: (config) => employeeServiceApi.get('/employees/products/sales',config),
+    getProductById: (id) => employeeServiceApi.get(`/employees/products/${id}`),
+    getCategories: (config) => employeeServiceApi.get('/employees/categories',config),
+    getBrands: (config) => employeeServiceApi.get('/employees/brands',config),
+    getUsers: (config) => employeeServiceApi.get('/employees/users',config),
+    deleteUsers: (id) => employeeServiceApi.delete(`/users/${id}`),
+    deleteEmployees: (id) => employeeServiceApi.delete(`/employees/${id}`),
+    getStaffs: (config) => employeeServiceApi.get('/employees/staffs',config),
+    createStaff: (config) => employeeServiceApi.post('/employees/register',config),
+    getOrders: (config) => employeeServiceApi.get('/employees/orders',config),
+    getOrdersById: (id) => employeeServiceApi.get(`/orders/${id}`),
+    updateOrder: (id,config) => employeeServiceApi.put(`/orders/${id}`, config),
+    deleteOrders: (id) => employeeServiceApi.delete(`/orders/${id}`),
+    postProducts: (productData) => employeeServiceApi.post('/employees/products', productData,
         {
             headers: {
               'Content-Type': 'multipart/form-data',
             },
         }
     ),
-    updateSpecifications: (productId, specifications) => api.put(`/specifications/${productId}`, 
+    updateSpecifications: (productId, specifications) => employeeServiceApi.put(`/specifications/${productId}`, 
         { specifications },
         {
             headers: {
@@ -126,13 +169,25 @@ export const adminService = {
             },
         }
     ),
-    updateVariants: (id, variants) => api.put(`/variants/${id}`,{variants}),
-    updateUser: (userId, user) => api.put(`/users/${userId}`,user,
+    updateVariants: (variants) => employeeServiceApi.put(`/variants`,{variants}),
+    createVariant: (variant) => employeeServiceApi.post(`/variants/create`,{variant}),
+    deleteVariant: (variant) => employeeServiceApi.delete(`/variants/${variant}`),
+    updateUser: (userId, user) => employeeServiceApi.put(`/users/${userId}`,user,
         {
             headers: {
               'Content-Type': 'multipart/form-data',
             },
         }
     ),
-    deleteReview: (review) => api.delete(`/reviews/${review}`),
+    updateStaff: (userId, user) => employeeServiceApi.put(`/employees/${userId}`,user,
+        {
+            headers: {
+              'Content-Type': 'multipart/form-data',
+            },
+        }
+    ),
+    getStatisticOrders: () => employeeServiceApi.get(`/orders/statisticProduct`),
+    getReviews: (config) => employeeServiceApi.get(`/reviews`,config),
+    deleteReview: (review) => employeeServiceApi.delete(`/reviews/${review}`),
+    getAllBils: (config) => employeeServiceApi.get(`/bills`,config),
 }
